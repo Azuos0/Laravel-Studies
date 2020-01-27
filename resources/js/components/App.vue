28109b17e3ac433860ef1a057ea79b61ed1b36fe
Igor Vieira <igorvieiradsz@gmail.com>
2020-01-27 00:10:54 -0300

    Começando o projeto e adicionando o Vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+        <h3>Hello new World! \o/</h3>
+        <button class="btn btn-secondary">it's working</button>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
